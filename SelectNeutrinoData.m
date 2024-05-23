1d847f5cd0e090f5da014bf83c070b88dc42619a
WANG Chao <zhikaiyici@hotmail.com>
2024-05-23 13:13:15 +0800

    Update

--- SelectNeutrinoData.m
+++ SelectNeutrinoData.m
@@ -11,23 +11,25 @@ IBDEvent = cell(fileNum, 5);
 timeGap = [];
 muonEvent = [];
 muonFamily = cell(fileNum, 1);
-for ii = 1:fileNum
-% parfor ii = 1:fileNum
+% for ii = 1:fileNum
+parfor ii = 1:fileNum
     fileName = [dirName, '\', name(ii,:)];
     while contains(fileName, ' ')
         fileName = strrep(fileName, ' ', '');
     end
     temp = load(fileName);
     event = temp.event;
-    [muonE, muonF, timeG] = MuonSelection(event);
-    timeGap = [timeGap; timeG];
-    % muonEvent = [muonEvent; muonE];
-    muonFamily{ii, 1} = muonF;
+    % [muonE, muonF, timeG] = MuonSelection(event);
+    % timeGap = [timeGap; timeG];
+    % % muonEvent = [muonEvent; muonE];
+    % muonFamily{ii, 1} = muonF;
     % ParSave(['muon_corrected\muon_', fileName(end-15:end-4)], muonE, '-v7.3');
     temp = IBDSelection(event);
     IBDNum = size(temp.ID, 1);
     eventNum = size(event.ID, 1);
     IBDEvent(ii,:) ={fileName(end-15:end-4), temp, IBDNum, eventNum, IBDNum ./ eventNum};
 end
-% save("IBDEvent_P25D28-s.mat", "IBDEvent", '-v7');
+save("IBDEvent_P25D28_muonic.mat", "IBDEvent", '-v7');
+% save("MuonFamily_1st6.mat", "muonFamily", '-v7');
+% save("MuonNeutronTimeGap_1st6.mat", "timeGap", '-v7');
 
